5b652201315e1c84677a436cfc0d3ce7c8437413
Zarif Samar <Samar.Zarif@sanlam.co.za>
2025-09-24 11:29:25 +0100

    refactor: add CloudFormation stack module and consolidate configs

---
+++ terraform/cicd-deployment/modules/cloudformation_stack/main.tf
@@ -0,0 +1,21 @@
+resource "aws_cloudformation_stack" "dashboard" {
+  count = var.config.enabled ? 1 : 0
+
+  name         = var.config.stack_name
+  template_url = var.config.template_url
+  capabilities = var.config.capabilities
+  parameters   = var.config.parameters
+
+  timeouts {
+    create = var.config.timeouts.create
+    update = var.config.timeouts.update
+    delete = var.config.timeouts.delete
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [tags]
+  }
+
+  tags = var.config.tags
+}
