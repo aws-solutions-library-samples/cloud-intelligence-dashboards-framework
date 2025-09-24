8228df3cc841f83f1ef5b69024f512edf613429e
Zarif Samar <Samar.Zarif@sanlam.co.za>
2025-09-24 11:29:25 +0100

    refactor: add CloudFormation stack module and consolidate configs

--- terraform/cicd-deployment/dashboards.tf
+++ terraform/cicd-deployment/dashboards.tf
@@ -1,308 +1,96 @@
-resource "aws_cloudformation_stack" "cid_dataexports_destination" {
-  # checkov:skip=CKV_AWS_124:SNS topic not required for this use case
-  provider     = aws.destination_account
-  name         = "CID-DataExports-Destination"
-  template_url = local.template_urls.data_exports
-  capabilities = local.common_capabilities
+# Foundational stacks - These are foundational stacks and should exist first
+# They provide the core infrastructure required for all dashboards
 
-  parameters = {
-    DestinationAccountId = var.global_values.destination_account_id
-    ResourcePrefix       = var.cid_dataexports_destination.resource_prefix
-    ManageCUR2           = var.cid_dataexports_destination.manage_cur2
-    ManageFOCUS          = var.cid_dataexports_destination.manage_focus
-    ManageCOH            = var.cid_dataexports_destination.manage_coh
-    SourceAccountIds     = var.global_values.source_account_ids
-    EnableSCAD           = var.cid_dataexports_destination.enable_scad
-    RolePath             = var.cid_dataexports_destination.role_path
-    TimeGranularity      = var.cid_dataexports_destination.time_granularity
+# 1. Data exports destination stack - Creates destination account resources
+module "cid_dataexports_destination" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  config = local.foundational_stacks.cid_dataexports_destination
 }
 
-
-resource "aws_cloudformation_stack" "cid_dataexports_source" {
-  # checkov:skip=CKV_AWS_124:SNS topic not required for this use case
-  depends_on = [
-    aws_cloudformation_stack.cid_dataexports_destination
-  ]
-  name         = "CID-DataExports-Source"
-  provider     = aws
-  template_url = local.template_urls.data_exports
-  capabilities = local.common_capabilities
-
-  parameters = {
-    DestinationAccountId = var.global_values.destination_account_id
-    ResourcePrefix       = var.cid_dataexports_source.source_resource_prefix
-    ManageCUR2           = var.cid_dataexports_source.source_manage_cur2
-    ManageFOCUS          = var.cid_dataexports_source.source_manage_focus
-    ManageCOH            = var.cid_dataexports_source.source_manage_coh
-    SourceAccountIds     = var.global_values.source_account_ids
-    EnableSCAD           = var.cid_dataexports_source.source_enable_scad
-    RolePath             = var.cid_dataexports_source.source_role_path
-    TimeGranularity      = var.cid_dataexports_source.source_time_granularity
+# 2. Data exports source stack - Creates source account resources (conditional)
+module "cid_dataexports_source" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  config     = local.foundational_stacks.cid_dataexports_source
+  depends_on = [module.cid_dataexports_destination]
 }
 
+# 3. Cloud Intelligence Dashboards - Main dashboard stack with foundational dashboards
+module "cloud_intelligence_dashboards" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
+  }
 
-resource "aws_cloudformation_stack" "cloud_intelligence_dashboards" {
-  # checkov:skip=CKV_AWS_124:SNS topic not required for this use case
-  name     = "Cloud-Intelligence-Dashboards"
-  provider = aws.destination_account
+  config = local.foundational_stacks.cloud_intelligence_dashboards
   depends_on = [
-    aws_cloudformation_stack.cid_dataexports_source,
-    aws_cloudformation_stack.cid_dataexports_destination
+    module.cid_dataexports_source,
+    module.cid_dataexports_destination
   ]
-  capabilities = local.common_capabilities
-  template_url = local.template_urls.cudos
-
-  parameters = {
-    # Prerequisites Variables
-    PrerequisitesQuickSight            = var.cloud_intelligence_dashboards.prerequisites_quicksight
-    PrerequisitesQuickSightPermissions = var.cloud_intelligence_dashboards.prerequisites_quicksight_permissions
-    QuickSightUser                     = var.global_values.quicksight_user
-    LakeFormationEnabled               = var.cloud_intelligence_dashboards.lake_formation_enabled
-
-    # CUR Parameters
-    CURVersion                      = var.cloud_intelligence_dashboards.cur_version
-    DeployCUDOSv5                   = var.dashboards.cudos_v5
-    DeployCostIntelligenceDashboard = var.dashboards.cost_intelligence
-    DeployKPIDashboard              = var.dashboards.kpi
-
-    # Optimization Parameters
-    OptimizationDataCollectionBucketPath = var.cloud_intelligence_dashboards.optimization_data_collection_bucket_path
-    DeployTAODashboard                   = var.cloud_intelligence_dashboards.deploy_tao_dashboard
-    DeployComputeOptimizerDashboard      = var.cloud_intelligence_dashboards.deploy_compute_optimizer_dashboard
-    PrimaryTagName                       = var.cloud_intelligence_dashboards.primary_tag_name
-    SecondaryTagName                     = var.cloud_intelligence_dashboards.secondary_tag_name
-
-    # Technical Parameters
-    AthenaWorkgroup                  = var.cloud_intelligence_dashboards.athena_workgroup
-    AthenaQueryResultsBucket         = var.cloud_intelligence_dashboards.athena_query_results_bucket
-    DatabaseName                     = var.cloud_intelligence_dashboards.database_name
-    GlueDataCatalog                  = var.cloud_intelligence_dashboards.glue_data_catalog
-    Suffix                           = var.cloud_intelligence_dashboards.suffix
-    QuickSightDataSourceRoleName     = var.cloud_intelligence_dashboards.quicksight_data_source_role_name
-    QuickSightDataSetRefreshSchedule = var.cloud_intelligence_dashboards.quicksight_data_set_refresh_schedule
-    LambdaLayerBucketPrefix          = var.cloud_intelligence_dashboards.lambda_layer_bucket_prefix
-    DeployCUDOSDashboard             = var.cloud_intelligence_dashboards.deploy_cudos_dashboard
-    DataBucketsKmsKeysArns           = var.cloud_intelligence_dashboards.data_buckets_kms_keys_arns
-    DeploymentType                   = var.cloud_intelligence_dashboards.deployment_type
-    ShareDashboard                   = var.cloud_intelligence_dashboards.share_dashboard
-
-    # Legacy Parameters
-    KeepLegacyCURTable  = var.cloud_intelligence_dashboards.keep_legacy_cur_table
-    CURBucketPath       = var.cloud_intelligence_dashboards.cur_bucket_path
-    CURTableName        = var.cloud_intelligence_dashboards.cur_table_name
-    PermissionsBoundary = var.cloud_intelligence_dashboards.permissions_boundary
-    RolePath            = var.cloud_intelligence_dashboards.role_path
-  }
-
-  timeouts {
-    create = "60m"
-    update = "60m"
-    delete = "60m"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes = [
-      # Ignore changes to tags
-      tags
-    ]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Foundational"
-    DashboardId   = "cloud-intelligence-dashboards"
-  })
 }
 
 # Additional CUR-based dashboards - Sequential deployment
-resource "aws_cloudformation_stack" "trends_dashboard" {
-  count = var.dashboards.trends == "yes" ? 1 : 0
+# These are optional dashboards that deploy after foundational stacks
+# They must deploy sequentially to avoid CloudFormation conflicts
 
-  name         = "Trends-Dashboard"
-  provider     = aws.destination_account
-  template_url = local.template_urls.cid_plugin
-  capabilities = local.common_capabilities
-
-  depends_on = [
-    aws_cloudformation_stack.cloud_intelligence_dashboards
-  ]
-
-  parameters = {
-    DashboardId = "trends-dashboard"
+# 1. Trends Dashboard - Daily anomaly detection and cost trends analysis
+module "trends_dashboard" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [tags]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Additional"
-    DashboardId   = "trends-dashboard"
-  })
+  config     = local.additional_dashboards.trends
+  depends_on = [module.cloud_intelligence_dashboards]
 }
 
-resource "aws_cloudformation_stack" "datatransfer_dashboard" {
-  count = var.dashboards.datatransfer == "yes" ? 1 : 0
-
-  name         = "DataTransfer-Cost-Analysis-Dashboard"
-  provider     = aws.destination_account
-  template_url = local.template_urls.cid_plugin
-  capabilities = local.common_capabilities
-
-  depends_on = [
-    aws_cloudformation_stack.cloud_intelligence_dashboards,
-    aws_cloudformation_stack.trends_dashboard
-  ]
-
-  parameters = {
-    DashboardId = "datatransfer-cost-analysis-dashboard"
+# 2. Data Transfer Dashboard - Data transfer cost analysis and optimization
+module "datatransfer_dashboard" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [tags]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Additional"
-    DashboardId   = "datatransfer-cost-analysis-dashboard"
-  })
+  config     = local.additional_dashboards.datatransfer
+  depends_on = [module.trends_dashboard]
 }
 
-resource "aws_cloudformation_stack" "marketplace_dashboard" {
-  count = var.dashboards.marketplace == "yes" ? 1 : 0
-
-  name         = "AWS-Marketplace-SPG-Dashboard"
-  provider     = aws.destination_account
-  template_url = local.template_urls.cid_plugin
-  capabilities = local.common_capabilities
-
-  depends_on = [
-    aws_cloudformation_stack.cloud_intelligence_dashboards,
-    aws_cloudformation_stack.datatransfer_dashboard
-  ]
-
-  parameters = {
-    DashboardId            = "aws-marketplace"
-    RequiresDataCollection = "no"
+# 3. Marketplace Dashboard - AWS Marketplace spend analysis
+module "marketplace_dashboard" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [tags]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Additional"
-    DashboardId   = "aws-marketplace"
-  })
+  config     = local.additional_dashboards.marketplace
+  depends_on = [module.datatransfer_dashboard]
 }
 
-resource "aws_cloudformation_stack" "connect_dashboard" {
-  count = var.dashboards.connect == "yes" ? 1 : 0
-
-  name         = "Amazon-Connect-Cost-Insight-Dashboard"
-  provider     = aws.destination_account
-  template_url = local.template_urls.cid_plugin
-  capabilities = local.common_capabilities
-
-  depends_on = [
-    aws_cloudformation_stack.cloud_intelligence_dashboards,
-    aws_cloudformation_stack.marketplace_dashboard
-  ]
-
-  parameters = {
-    DashboardId = "amazon-connect-cost-insight-dashboard"
+# 4. Connect Dashboard - Amazon Connect cost insights and usage analysis
+module "connect_dashboard" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [tags]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Additional"
-    DashboardId   = "amazon-connect-cost-insight-dashboard"
-  })
+  config     = local.additional_dashboards.connect
+  depends_on = [module.marketplace_dashboard]
 }
 
-resource "aws_cloudformation_stack" "containers_dashboard" {
-  count = var.dashboards.containers == "yes" ? 1 : 0
-
-  name         = "SCAD-Containers-Dashboard"
-  provider     = aws.destination_account
-  template_url = local.template_urls.cid_plugin
-  capabilities = local.common_capabilities
-
-  depends_on = [
-    aws_cloudformation_stack.cloud_intelligence_dashboards,
-    aws_cloudformation_stack.connect_dashboard
-  ]
-
-  parameters = {
-    DashboardId = "scad-containers-cost-allocation"
+# 5. Containers Dashboard - SCAD containers cost allocation and optimization
+module "containers_dashboard" {
+  source = "./modules/cloudformation_stack"
+  providers = {
+    aws = aws.destination_account
   }
 
-  timeouts {
-    create = lookup(local.default_timeouts, "create", "30m")
-    update = lookup(local.default_timeouts, "update", "30m")
-    delete = lookup(local.default_timeouts, "delete", "30m")
-  }
-
-  lifecycle {
-    create_before_destroy = true
-    ignore_changes        = [tags]
-  }
-
-  tags = merge(local.common_tags, {
-    DashboardType = "Additional"
-    DashboardId   = "scad-containers-cost-allocation"
-  })
+  config     = local.additional_dashboards.containers
+  depends_on = [module.connect_dashboard]
 }
